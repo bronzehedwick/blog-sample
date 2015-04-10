b9a78287d8c28e78d307018cf51651dc3c1595f7
Chris DeLuca <bronzehedwick@gmail.com>
2015-04-09 21:37:57 -0400

    Add publication date parsing.
    Also fix the date format; use RFC instead of unix timestamp.

--- src/js/main.js
+++ src/js/main.js
@@ -12,15 +12,20 @@
           var articleEl = document.createElement('article'),
               headingEl = document.createElement('heading'),
               h2El      = document.createElement('h2'),
+              timeEl    = document.createElement('time'),
               imgEl     = document.createElement('img'),
               pEl       = document.createElement('p'),
-              mainEl    = document.getElementById('main');
+              mainEl    = document.getElementById('main'),
+              date      = new Date(posts[post].date);
 
           h2El.innerHTML = posts[post].title;
+          timeEl.setAttribute('datetime', date);
+          timeEl.innerHTML = (date.getMonth() + 1) + '/' + date.getDay() + '/' + date.getFullYear();
           imgEl.src = posts[post].image;
           pEl.innerHTML = posts[post].description;
 
           headingEl.appendChild(h2El);
+          headingEl.appendChild(timeEl);
           articleEl.appendChild(headingEl);
           articleEl.appendChild(imgEl);
           articleEl.appendChild(pEl);
