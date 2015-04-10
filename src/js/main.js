06e1b2d8e186fa49c54f5742ee8daf920f195fd1
Chris DeLuca <bronzehedwick@gmail.com>
2015-04-09 22:24:22 -0400

    Add linking via pushstate.

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,59 @@
 (function sampleBlog() {
   'use strict';
 
+  function postLink(link, post) {
+    link.addEventListener('click', function(e) {
+      var mainEl = document.getElementById('main');
+      mainEl.setAttribute('style', 'display:none;');
+      mainEl.innerHTML = '';
+      history.pushState(null, null, link.href);
+      e.preventDefault();
+      parsePost(post, link.href);
+    }, false);
+  }
+
+  function parsePost(post, id) {
+    var articleEl = document.createElement('article'),
+      headingEl = document.createElement('heading'),
+      h2El      = document.createElement('h2'),
+      aEl       = document.createElement('a'),
+      timeEl    = document.createElement('time'),
+      imgEl     = document.createElement('img'),
+      pEl       = document.createElement('p'),
+      mainEl    = document.getElementById('main'),
+      date      = new Date(post.date);
+
+    aEl.href = id;
+    postLink(aEl, post);
+    h2El.innerHTML = post.title;
+    timeEl.setAttribute('datetime', date);
+    timeEl.innerHTML = (date.getMonth() + 1) + '/' + date.getDay() + '/' + date.getFullYear();
+    imgEl.src = post.image;
+    pEl.innerHTML = post.description;
+
+    aEl.appendChild(h2El);
+    headingEl.appendChild(aEl);
+    headingEl.appendChild(timeEl);
+    articleEl.appendChild(headingEl);
+    articleEl.appendChild(imgEl);
+    articleEl.appendChild(pEl);
+
+    mainEl.appendChild(articleEl);
+    mainEl.setAttribute('style', 'display:block;');
+  }
+
   function parsePosts(posts) {
-    if (window.location.hash) {
-      // We are on a single post page
-    }
-    else {
-      // We are on the main page
-      for (var post in posts) {
-        if (posts.hasOwnProperty(post)) {
-          var articleEl = document.createElement('article'),
-              headingEl = document.createElement('heading'),
-              h2El      = document.createElement('h2'),
-              timeEl    = document.createElement('time'),
-              imgEl     = document.createElement('img'),
-              pEl       = document.createElement('p'),
-              mainEl    = document.getElementById('main'),
-              date      = new Date(posts[post].date);
-
-          h2El.innerHTML = posts[post].title;
-          timeEl.setAttribute('datetime', date);
-          timeEl.innerHTML = (date.getMonth() + 1) + '/' + date.getDay() + '/' + date.getFullYear();
-          imgEl.src = posts[post].image;
-          pEl.innerHTML = posts[post].description;
-
-          headingEl.appendChild(h2El);
-          headingEl.appendChild(timeEl);
-          articleEl.appendChild(headingEl);
-          articleEl.appendChild(imgEl);
-          articleEl.appendChild(pEl);
-
-          mainEl.appendChild(articleEl);
-          mainEl.setAttribute('style', 'display:block;');
-        }
+    for (var post in posts) {
+      if (posts.hasOwnProperty(post)) {
+        parsePost(posts[post], post);
       }
     }
+
+    window.onpopstate = function(e) {
+      var mainEl = document.getElementById('main');
+      mainEl.innerHTML = '';
+      parsePosts(posts);
+    };
   }
 
   // Fetch post data
